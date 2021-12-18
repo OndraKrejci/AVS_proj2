9aca15371906b57ea8b8f51b96cc7614f5ccc51d
OndraKrejci <o.krejci13@gmail.com>
2021-12-18 14:12:38 +0100

    Zip script

---
+++ zip.sh
@@ -0,0 +1,1 @@
+zip -j xkrejc69.zip parallel_builder/loop_mesh_builder.cpp parallel_builder/loop_mesh_builder.h parallel_builder/tree_mesh_builder.cpp parallel_builder/tree_mesh_builder.h PMC-xkrejc69.txt input_scaling_strong.png input_scaling_weak.png grid_scaling.png
